b0fa9a96c47665ca27621815cfac876e60310b78
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2021-10-14 13:05:31 +0800

    update

--- choropleth_map.js
+++ choropleth_map.js
@@ -1,4 +1,4 @@
-var vg_1 = "choropleth_map.json";
-vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+var vg_1 = "map2.json";
+vegaEmbed("#choropleth_map", vg_1,{"actions": false}).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
