70ba7d7337710e7d8c8191af6fe97c722902d164
GeorgeTan615 <87482146+GeorgeTan615@users.noreply.github.com>
2021-10-17 12:05:01 +0800

    Delete streamgraph.js

--- streamgraph.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "streamgraph.json";
-vegaEmbed("#streamgraph", vg_1,{"actions": false}).then(function(result) {
- // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
